84fa5482a286cefce6aa52775f15fe6ba462e6c6
officiallyAkshayAgrawal <akshay.space9@gmail.com>
2017-11-30 22:18:11 -0800

    testing testing

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='crickit',
-    version='0.0.10d-dev',
+    version='0.0.1011-dev',
     packages=['crickit'],
     url="https://github.com/oficiallyAkshayEdu/crickit",
     license='MITa',
     author='Akshay Agrawal',
     author_email='',
-    description='Text based Cricket Simulator in Python'
+    description='Text based Cricket Simulator in Python',
+    long_description = readme()
 )
